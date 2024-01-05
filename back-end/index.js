c419a5f532463edb32128e8e03f4ac58c1e3e3fd
vincent <vincent@trakmy.fr>
2024-01-05 16:25:14 +0100

    login back

--- back-end/index.js
+++ back-end/index.js
@@ -49,20 +49,29 @@ app.post("/register", async (req, res) => {
 app.post("/login", async (req, res) => {
   const { username, password } = req.body;
 
+  // Chercher l'utilisateur dans la base de données
   db.query(
     "SELECT * FROM users WHERE username = ?",
     [username],
     async (err, results) => {
-      if (err) throw err;
+      if (err) {
+        return res
+          .status(500)
+          .send("Erreur lors de la recherche de l'utilisateur");
+      }
 
       if (results.length > 0) {
+        // Comparer les mots de passe
         const comparison = await bcrypt.compare(password, results[0].password);
+        console.log(comparison);
         if (comparison) {
-          req.session.userId = results[0].id; // ou un autre identifiant unique
-          return res.redirect("/dashboard"); // Redirigez vers une page sécurisée
+          return res.status(200).send("Connexion réussie");
+        } else {
+          return res.status(400).send("Mot de passe incorrect");
         }
+      } else {
+        return res.status(404).send("Utilisateur non trouvé");
       }
-      res.redirect("/login"); // Redirigez vers la page de connexion en cas d'échec
     }
   );
 });
